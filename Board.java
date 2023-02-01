e23e8e35cce7a5ca20e0ac6aedf529f2f3f370f4
Yusef McCaskill <yusef.mccaskill@mrjaffesclass.com>
2023-02-01 14:23:09 -0800

    inital commit

--- Board.java
+++ Board.java
@@ -3,9 +3,36 @@ public class Board
 {
     private int rows = 6;
     private int columns = 7;
-    
+
     Piece [][]ourBoard = new Piece[rows][columns];
-    
+
+    public boolean addPiece(int rowToAdd, String color) {
+        // within normal range 
+        if(rowToAdd >= 0 && rowToAdd < rows) {
+            // we can add
+            if(ourBoard[rowToAdd][0] == null) {
+                boolean addedThePiece = false;
+                for(int col = columns -1; col >= 0; col--) { 
+                    if(ourBoard[rowToAdd][col] == null) {
+                        ourBoard[rowToAdd][col] = new Piece();
+                        ourBoard[rowToAdd][col].setColor(color);
+                        addedThePiece = true; 
+                        break;
+                    }
+                }
+                return addedThePiece;
+            } else {
+                //that row is full
+                System.err.println("This row is full, please choose another.");
+                return false;
+            }
+        } else {
+            //outsude normal range
+            System.err.println("You are trying to add somewhere that is not supported.");
+            return false;
+        }
+    }
+
     public void printBoard() {
         for (int row = 0; row < rows; row++) {
             System.out.print("|");
@@ -20,7 +47,7 @@ public class Board
             System.out.println();
         }
     }
-    
+
     public Board() {
         for (int row = 0; row < rows; row++) {
             for(int col = 0; col < columns; col++) {
@@ -28,5 +55,5 @@ public class Board
             }
         }
     }
-    
+
 }
