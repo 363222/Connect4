6d32051c59e7be97d5d497e96f3a1e850d264131
Yusef McCaskill <yusef.mccaskill@mrjaffesclass.com>
2023-02-02 14:24:05 -0800

    intial commit

--- Game.java
+++ Game.java
@@ -1,11 +1,23 @@
 
 public class Game {
     private Board board;
+    private String color1;
+    private String color2;
     
+    //true if player1's turn
+    //false if player2's turn
+    private boolean is1playing;
     
+    public_Game(String color1, String color2) {
+        this.board = new Board();
+        this.color1 = color1;
+        this.color2 = color2;
+        
+        is1playing = (new Random()).nextBoolean();
+    }
     
-    public_Game() {
-        board = new Board();
+    public void startGame() {
+        
     }
 }
    
